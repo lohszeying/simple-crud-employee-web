d223bf43b3998f6ba69b7ad5c39486a17c57760d
Loh Sze Ying <szeying@mavericks-consulting.com>
2022-09-01 11:06:41 +0800

    Placeholder card button added (UI)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,11 +5,17 @@ import classes from "./Card.module.css";
 const Card = () => {
   return (
     <React.Fragment>
-      <div className={classes.container}>
-        <div className={classes.card}>
+      <div className={classes["card-container"]}>
+        <div className={classes["employee-details"]}>
           <h1 className={classes.name}>Name 1</h1>
-          <p>HR</p>
-          <p>$1000</p>
+          <div className={classes.description}>
+            <p>HR</p>
+            <p>$1000</p>
+          </div>
+        </div>
+        <div className={classes["button-group"]}>
+          <button>Edit</button>
+          <button>Delete</button>
         </div>
       </div>
     </React.Fragment>
