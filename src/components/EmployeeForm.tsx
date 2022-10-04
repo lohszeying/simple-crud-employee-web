b994d8e7a13aba84b6a3f7dd06732097b61d4dc0
Loh Sze Ying <szeying@mavericks-consulting.com>
2022-10-04 14:23:31 +0800

    Add employee form frontend validation

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -1,16 +1,19 @@
-import React, { Fragment, useRef } from "react";
+import React, { Fragment, MutableRefObject, useRef } from "react";
 import { RefactorActionInfo } from "typescript";
 import classes from "./EmployeeForm.module.css";
 
 const EmployeeForm = (props: any) => {
-  const authorInputRef = useRef();
-  const textInputRef = useRef();
+  // const nameInputRef = useRef<HTMLInputElement>(null);
+  const nameInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const salaryInputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const departmentInputRef = useRef() as MutableRefObject<HTMLSelectElement>;
 
   const  submitFormHandler = (event: React.FormEvent) => {
     event.preventDefault();
-
-    // const enteredAuthor = authorInputRef.current.value;
-    // const enteredText = textInputRef.current.value;
+    
+    const enteredName = nameInputRef.current.value;
+    const enteredSalary = salaryInputRef.current.value;
+    const enteredDepartment = departmentInputRef.current.value;
 
     // optional: Could validate here
 
@@ -28,16 +31,28 @@ const EmployeeForm = (props: any) => {
         onSubmit={submitFormHandler}
       >
         <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author"  />
+          <label htmlFor="name">Name</label>
+          {/* https://stackoverflow.com/questions/69157200/minlength-doesnt-work-in-typescript-react */}
+          <input type="text" id="name" minLength={4} maxLength={30} required={true} ref={nameInputRef} placeholder="John Doe" />
         </div>
+
         <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" ></textarea>
+          <label htmlFor="salary">Salary</label>
+          <input type="number" id="number" min="0" required={true} ref={salaryInputRef} placeholder="2500" />
         </div>
+
+        {/* Department should be dropdown */}
+        <div className={classes.control}>
+          <label htmlFor="name">Department</label>
+          <select id="department" ref={departmentInputRef} required={true} >
+            <option value="HR">HR</option>
+            <option value="PS">PS</option>
+          </select>
+        </div>
+
         <div className={classes.actions}>
           <button onClick={finishEnteringHandler} className="btn">
-            Add Quote
+            Add Employee
           </button>
         </div>
       </form>
