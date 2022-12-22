07fab893f77a65d2692fe0488cc9c835470ab556
Loh Sze Ying <szeying@mavericks-consulting.com>
2022-12-22 17:10:27 +0800

    changed to redirect to Login component if not logged in in homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Employees from "../components/Employees";
 import { RootState } from "../store";
 import { useSelector } from "react-redux";
@@ -11,13 +11,14 @@ import { checkLogin } from "../store/user-slice";
 import {toast} from 'react-toastify'
 import { Status } from "../model/status";
 import { useHistory } from "react-router-dom";
+import { ReactNode } from "react";
 
 const Homepage = () => {
   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
 
   // const getStatus = useSelector((state: RootState) => state.user.status);
   // const getErrorMsg = useSelector((state: RootState) => state.user.errorMsg);
-  // const history = useHistory();
+  const history = useHistory();
 
   // useEffect(() => {
   //   if (getStatus === Status.REJECTED) {
@@ -27,8 +28,14 @@ const Homepage = () => {
   //   }
   // }, [getStatus])
 
+  const routeToLogin = () => {
+    history.push('/login');
+
+    return (<Fragment></Fragment>);
+  }
+
   return (<React.Fragment>
-    {isLoggedIn ? <Employees /> : <Login />}
+    {isLoggedIn ? <Employees /> : routeToLogin() }
   </React.Fragment>)
 };
 
